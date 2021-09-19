ee19e08c1d778943e6cca3c646b207ae9e1ea4f8
sree-07 <90963697+sree-07@users.noreply.github.com>
2021-09-19 12:45:37 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,80 @@
+"""arithmatic operaters"""
+
+# a=float(input("enter the value of a:"))
+# b=float(input("enter the value of b:"))
+# print(f"the value of {a}+{b} is {a+b}")#the value of 23.0+23.0 is 46.0
+# print(f"the value of {a}-{b} is {a-b}")#the value of 23.0-23.0 is 0.0
+# print(f"the value of {a}*{b} is {a*b}")#the value of 23.0*23.0 is 529.0
+# print(f"the value of {a}/{b} is {a/b}")#the value of 23.0/23.0 is 1.0
+
+# print(f"the value of {a}%{b} is {a%b}")#the value of 23.0%23.0 is 0.0
+# print(f"the value of {a}**{b} is {a**b}")#the value of 23.0**23.0 is 2.088046799984791e+31
+
+
+
+"""floor division"""
+# c=4//2
+# print(c)#2
+# d=4//2.0
+# print(d)#2.0
+# e=5/2
+# print(e)#2.5
+# f=4.0//2
+
+
+"""assignment operator"""
+# a=12
+# print(a)#12
+# b=13
+# print(b)#13
+# a=a+b
+# print(a)#25
+# a=a+a
+# print(a)#50
+
+# a=10
+# a+=5
+# print(a)#15
+# a-=2
+# print(a)#13
+# a*=4
+# print(a)#52
+# a/=2
+# print(a)#26.0
+# a**=2
+# print(a)#676.0
+
+# p=int(input("enter the value of p:"))
+# print(f"the value of p  is {p}")#the value of p  is 22
+
+# p+=2
+# print(f"the value of p after p+=2 is {p}")the value of p after p+=2 is 24
+
+# p-=2
+# print("the value of p after p-=2 is {}".format(p))the value of p after p-=2 is 22
+
+# p*=2
+# print("the value of p after p*=2 is {}".format(p))the value of p after p*=2 is 44
+# p/=2
+# print("the value of p after p/=2 is {}".format(p))the value of p after p/=2 is 22.0
+# p**=2
+# print("the value of p after p**=2 is {}".format(p))the value of p after p**=2 is 484.0
+
+
+"""Relational/comparition operator"""
+
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+print(f"the value of {a}<{b}is{a<b}")#the value of 2<3isTrue
+print(f"the value of {a}>{b}is{a>b}")#the value of 2>3isFalse
+print(f"the value of {a}<={b}is{a<=b}"#)the value of 2<=3isTrue
+print(f"the value of {a}>={b}is{a>=b}")#the value of 2>=3isFalse
+print(f"the value of {a}=={b}is{a==b}")#the value of 2==3isFalse
+print(f"the value of {a}!={b}is{a!=b}")#the value of 2!=3isTrue
+
+
+
+
+
+
+
