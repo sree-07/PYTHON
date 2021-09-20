84ae92d22f4d9c5b7c303bb40992dda9eba5997f
sree-07 <90963697+sree-07@users.noreply.github.com>
2021-09-20 11:21:25 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -63,18 +63,74 @@
 
 """Relational/comparition operator"""
 
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# print(f"the value of {a}<{b}is{a<b}")#the value of 2<3isTrue
+# print(f"the value of {a}>{b}is{a>b}")#the value of 2>3isFalse
+# print(f"the value of {a}<={b}is{a<=b}"#)the value of 2<=3isTrue
+# print(f"the value of {a}>={b}is{a>=b}")#the value of 2>=3isFalse
+# print(f"the value of {a}=={b}is{a==b}")#the value of 2==3isFalse
+# print(f"the value of {a}!={b}is{a!=b}")#the value of 2!=3isTrue
+
+      
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# print("the value of %d<%d=%d"%(a,b,a<b))             #the value of 4<5=1
+# print("the value of a=%d,b=%d a>b=%d"%(a,b,a<b))     #the value of a=4,b=5 a>b=1
+# print("the value of a=%d,b=%d a>=b=%d"%(a,b,a>=b))   #the value of a=4,b=5 a>=b=0
+# print("the value of a=%d,b=%d a<=b=%d"%(a,b,a<=b))   #the value of a=4,b=5 a<=b=1
+# print("the value of a=%d,b=%d a>b=%d"%(a,b,a>b))     #the value of a=4,b=5 a>b=0 
+# print("the value of a=%d,b=%d a==b=%d"%(a,b,a==b))   #the value of a=4,b=5 a==b=0
+# print("the value of a=%d,b=%d a!=b=%d"%(a,b,a!=b))   #the value of a=4,b=5 a!=b=1
+
+
+# """logical operators"""
+# a=int(input("enter the value of a:"))
+# b=int(input("enter the value of b:"))
+# print(f"the value of {a<b} and {a>b} is{a and b}")       #the value of True and False is6
+# print(f"the value of {a<b} or {a>b} is{a or b}")         #the value of True or False is5
+# print(f"the value of {a<=b} or {a>=b} is{a or b}")       #the value of True or False is5
+# print(f"the value of {a!=b} or {a==b} is{a or b}")       #the value of True or False is5
+# print(f"the value of {a<b} after not is {not a<b}")      #the value of True after not is False
+# print(f"the value of {a==b} after not is {not a==b}")    #the value of False after not is True
+# print(f"the value of {a!=b} after not is {not a!=b}")    #the value of True after not is False
+
+"""bitwise operator"""
 a=int(input("enter the value of a:"))
 b=int(input("enter the value of b:"))
-print(f"the value of {a}<{b}is{a<b}")#the value of 2<3isTrue
-print(f"the value of {a}>{b}is{a>b}")#the value of 2>3isFalse
-print(f"the value of {a}<={b}is{a<=b}"#)the value of 2<=3isTrue
-print(f"the value of {a}>={b}is{a>=b}")#the value of 2>=3isFalse
-print(f"the value of {a}=={b}is{a==b}")#the value of 2==3isFalse
-print(f"the value of {a}!={b}is{a!=b}")#the value of 2!=3isTrue
+c=int(input("enter the value of c:"))
+print("the value of {} & {} is {}".format(a,b,a&b))           #the value of 5 & 6 is 4
+print("the value of {} | {} is {}".format(a,b,a|b))           #the value of 5 | 6 is 7
+print("the value of ~{} is {}".format(a,~a))                  #the value of ~5 is -6 
+print("the value of ~{} is {}".format(c,~c))                  #the value of ~7 is -8  
+
+"""  identity operator  """
+a=4;b=[5];c=6;d=a;e=5;f=[5]
+print(f"the value of {a} is {b}={a is b}")                        #the value of 4 is [5]=False
+print(f"the value of {a} is {c}={a is c}")                        #the value of 4 is 6=False 
+print(f"the value of {a} is {d}={a is d}")                        #the value of 4 is 4=True
+print(f"the value of {a} is {e}={a is e}")                        #the value of 4 is 5=False   
+print(f"the value of {b} is {f}={b is f}")                        #the value of [5] is [5]=False 
+print(f"the value of {a} is not {b}=",a is not b)                 #the value of 4 is not [5]= True 
+print(f"the value of {a} is not {c}=",a is not c)                 #the value of 4 is not 6= True  
+print(f"the value of {a} is not {d}=",a is not d)                 #the value of 4 is not 4= False  
+print(f"the value of {a} is not {e}=",a is not e)                 #the value of 4 is not 5= True  
+print(f"the value of {b} is not {f}=",b is not f)                 #the value of [5] is not [5]= True
 
 
-
-
-
-
-
+"""member ship operator"""
+n=[1,2.3,'python']
+print(n[0],type(n[0]))                                                 #1 <class 'int'>
+print(n,type(n))                                                       #[1, 2.3, 'python'] <class 'list'>
+print(n[1],type(n[1]))                                                 #2.3 <class 'float'>
+print(n[2],type(n[2]))                                                 #python <class 'str'>
+print(f"the value of {n[0]} is n is {n[0] in n}")                      #the value of 1 is n is True
+print(f"the value of {n[1]} is n is {n[1] in n}")                      #the value of 2.3 is n is True 
+print(f"the value of {n[2]} is n is {n[2] in n}")                      #the value of python is n is True  
+print(f"the value of {'pytho'} not in n is{'pytho'}")                  #the value of pytho not in n ispytho 
+print(f"the value of {'python'} not in n is{n[2] not in n}")           #the value of python not in n isFalse
+print(1 not in n )                                                     #False
+print(5 not in n)                                                      #True
+print(4.5 not in n)                                                    #True
+print('pytho'not in n)                                                 #True
+print('python'not in n)                                                #False
