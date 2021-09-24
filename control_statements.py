5b50862feaabac74af0a35bdf175a0db490100b5
sree-07 <90963697+sree-07@users.noreply.github.com>
2021-09-24 15:36:16 +0530

    Add files via upload

---
+++ control_statements.py
@@ -0,0 +1,25 @@
+a=int(input("a value is....:"))
+b=int(input("b value is....:"))
+# if(a<b):print("a lessthan b is true")     #a lessthan b is true
+# if(a<b):print("a lessthan b is true") 
+# if(a>b):print("a lessthan b is true") 
+# if(a<=b):print("a lessthan b is true") 
+# if(a>=b):print("a lessthan b is true") 
+# if(a==b):print("a lessthan b is true") 
+# if(a!=b):print("a lessthan b is true") 
+
+# if(a<b):
+#      print(f"{a} is lessthan {b} is{a<b}")
+
+# if(a>b):
+#       print("a is greater then b is True ")
+# else:
+#    print("a is greater then b is false")
+
+
+# if(a>b):print("a is greater than b")
+# elif(a<b):print("a is less than b")
+# elif(a>=b):print("a is greater than or equal to b")
+# elif(a<=b):print("a is less then or equal to b")
+# elif(a==b):print("a is same as b")
+# elif(a!=b):print("a is not equal to b")
