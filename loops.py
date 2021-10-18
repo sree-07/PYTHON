baa70bb5d6fd2ecaae6392dcf31490df92111583
sree-07 <90963697+sree-07@users.noreply.github.com>
2021-10-18 15:58:40 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,56 @@
+# a=1,2,3,4,5
+# # print(*a)   1 2 3 4 5
+# # print(a)   (1, 2, 3, 4, 5)  
+# for x in a:
+#     print(x)
+#     # print(x,end=" ")
+# print(x)
+
+
+
+# a=1,2,3,4,5
+# for i in range(10):
+#     print(i)
+
+
+
+# n=int(input("enter the value of n:"))
+# for i in range(n):
+
+#     print(i,end=' ')
+
+
+
+# for o in range(1,20,2):
+#    print(o,end=' ')   #1 3 5 7 9 11 13 15 17 19 
+# for o in range(0,20,2):
+#    print(o,end=' ')      #0 2 4 6 8 10 12 14 16 18 
+
+
+# for a in range(10,0,-1):
+#     print(a,end=' ')   #10 9 8 7 6 5 4 3 2 1
+
+
+""" for  reverse"""
+# n=[2,7,4,1.2,7.7,"hi","hlo",(1,2)]
+# for i in n[::-1]:
+#     print(i,end=',')
+
+a="python programming"
+# for b in a[::-1]:
+#     print(b,end='')
+
+# k=int(input("enter the starting value:"))
+# l=int(input("enter the ending value:"))
+# while(k<l):
+#     print(k,end=" ")
+#     k+=1
+
+k=int(input("enter the starting value:"))
+l=int(input("enter the ending value:"))
+while(k>l):
+    print(k,end=" ")
+    k-=1
+
+
+
